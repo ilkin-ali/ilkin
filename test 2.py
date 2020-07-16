7b32fcbc940be8e201ca14a3ceedd94539ee34e4
Ilkin Aliyev <ilkin.aliyev@gmail.com>
2020-07-17 03:43:49 +0400

    test2 v0

---
+++ test 2.py
@@ -0,0 +1,35 @@
+my_str = "hello world"
+print(my_str + "\n")
+print("Hi")
+print("\n")
+print("Hi")
+#####
+print("\n")
+#####
+print(type(4.7))
+#####
+print("\n")
+#####
+print(.1 + .1 + .1 == .300000)
+#####
+print("\n")
+#####
+this_string = 'Simon\'s skateboard is in the garage.'
+this_string2 = "Simon's skateboard is in the garage."
+print(this_string)
+print(this_string2)
+#####
+print("\n")
+#####
+first_word = 'Hello'
+second_word = 'There'
+print(first_word + ' ' + second_word)
+print(first_word[0])
+#####
+print("\n")
+#####
+username = "Kinari"
+timestamp = "04:50"
+url = "http://petshop.com/pets/mammals/cats"
+TODO=username+''+" accessed the site "+''+"url"+''+" at "+''+timestamp+"."
+print(TODO)
