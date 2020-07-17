d1d7b6866be847c503ef7481694377d1a026b60f
Ilkin Aliyev <ilkin.aliyev@gmail.com>
2020-07-17 04:11:37 +0400

    pys

---
+++ elif.py
@@ -0,0 +1,18 @@
+largest = None
+smallest = None
+while True:
+    try:
+        num = raw_input("Enter a number: ")
+        if num == "done":
+            break
+        print(num)
+        num = int(num)
+        if largest is None or largest < num:
+            largest = num
+        elif smallest is None or smallest > num:
+            smallest = num
+    except ValueError:
+        print("Please, enter only numbers.")
+
+print("Maximum", largest)
+print("Minimum", smallest)
