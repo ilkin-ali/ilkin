bb71cec9adc7aecae5f8c7dcf9fd038338b1be16
Ilkin Aliyev <ilkin.aliyev@gmail.com>
2020-07-23 01:05:42 +0400

    v1.1

--- Building_dictionaries.py
+++ Building_dictionaries.py
@@ -14,9 +14,14 @@ What's happening here?
 #When the if condition is not met, it is because thatword already exists in the word_counter dictionary, and the statement word_counter[word] = word_counter[word] + 1 increases the count of that word by 1.
 #Once the for loop finishes iterating through the list, the for loop is complete.
 
-#ikinci yolu da budur
+ikinci yolu da budur
 
 #for word in book_title:
   #  word_counter[word] = word_counter.get(word, 0) + 1
 #print(word_counter)
 
+What's happening here?
+The for loop iterates through the list as we saw earlier. The for loop feeds 'great' to the next statement in the body of the for loop.
+In this line: word_counter[word] = word_counter.get(word,0) + 1, since the key 'great' doesn't yet exist in the dictionary, get() will return the value 0 and word_counter[word] will be set to 1.
+Once it encounters a word that already exists in word_counter (e.g. the second appearance of 'the'), the value for that key is incremented by 1. On the second appearance of 'the', the key's value would add 1 again, resulting in 2.
+Once the for loop finishes iterating through the list, the for loop is complete
