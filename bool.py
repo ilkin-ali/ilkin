9fa82ae934d3990efc0dc0376da5254f8330511c
Ilkin Aliyev <ilkin.aliyev@gmail.com>
2020-09-08 13:13:42 +0400

    v1.0 Final

---
+++ bool.py
@@ -0,0 +1,11 @@
+def solve():
+    andd = hidden_operation("rand")
+    if hidden_operation('any') == False:
+        print('not')
+    elif andd == 'rand':
+        print('or')
+        print(hidden_operation('or'))
+    elif op
+    else:
+        print('and')
+        print(hidden_operation('and'))
